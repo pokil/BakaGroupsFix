8ba66bada9adff55f99db083e6006345d864ac10
pokil <cradfire@gmail.com>
2014-07-14 00:29:31 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,5 @@
 var tempGroup = {
-	"5248":"http://hereticlnt.blogspot.com/",
-	"6011":"http://yuriinwonderland.wordpress.com/",
-	"6015":"http://dh5alpha.blogspot.com/",
-	"6020":"http://hui3r.wordpress.com/",
-	"6022":"http://artificialdemons.boards.net/",
-	"6023":"http://ss-scans.blogspot.com/",
-	"6024":"http://www.batoto.net/group/_/r/rpapo-r4326",
-	"6028":"http://cicada-translation.tumblr.com/",
-	"6027":"http://tinyurl.com/animango-scans",
-	"6031":"http://editorfag.wordpress.com/",
-	"6032":"http://rinshankaihouscans.wordpress.com/"
+
 };
 
 var tempIRC = {
