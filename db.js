fb31e0c1b048818bd7d9512c9b3ee60b50b5ff18
pokil <cradfire@gmail.com>
2014-07-26 18:28:57 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 var tempGroup = {
-
+	"1935":"http://verdelish.tumblr.com/",
+	"6105":"http://gto14dis.blogspot.com/",
+	"6107":"http://raspomme.tumblr.com/",
+	"6108":"http://animefangirl.com/",
+	"6113":"http://balmunkfezarion.deviantart.com/",
+	"6117":"http://soukascans.blogspot.ca/"
 };
 
 var tempIRC = {
