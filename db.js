195e961fc769a70da7e0fdc6ab83771c33c3cd66
pokil <cradfire@gmail.com>
2014-08-03 19:20:08 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 var tempGroup = {
 	"1935":"http://verdelish.tumblr.com/",
+	"5960":"http://funnuraba.tumblr.com/",
+	"6087":"http://solitarytranslation.wordpress.com/",
 	"6105":"http://gto14dis.blogspot.com/",
 	"6107":"http://raspomme.tumblr.com/",
 	"6108":"http://animefangirl.com/",
 	"6113":"http://balmunkfezarion.deviantart.com/",
-	"6117":"http://soukascans.blogspot.ca/"
+	"6117":"http://soukascans.blogspot.ca/",
+	"6119":"http://flamenco-scanlations.tumblr.com/",
+	"6120":"http://tokkimori.weebly.com/",
+	"6124":"http://www.batoto.net/group/_/s/sunrise-scans-r4470",
+	"6129":"http://anarzeescan.wordpress.com/",
+	"6130":"http://magatamaphantom.site11.com/",
+	"6132":"http://breadingbad.tumblr.com/"
 };
 
 var tempIRC = {
