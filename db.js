7a23ae502d7af4f5b903cdcd2bba7aedf6f5d0a7
pokil <cradfire@gmail.com>
2014-09-05 15:51:43 +0700

    update

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 var tempGroup = {
-
+	
+	"6059":"http://hengao-scans.tk/",
+	"6173":"http://www.batoto.net/group/_/h/hazue-ishin-r3590",
+	"6186":"http://tensaitranslations.wordpress.com/"
 };
 
 var tempIRC = {
