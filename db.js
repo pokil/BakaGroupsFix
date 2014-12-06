d1e1213e19a17ba73da671e3ee4fe54031e91313
pokil <cradfire@gmail.com>
2014-12-06 23:42:45 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,25 @@
 var tempGroup = 
-
+	"5777":"https://www.facebook.com/Midnightdawnscans",
+	"6157":"http://finemang.comuf.com/",
+	"6198":"http://bato.to/group/_/t/tsuki-to-chikyuu-translations-r4673",
+	"6208":"https://drive.google.com/open?id=0B7PIRwu9hTEFODllMFFRemljMGc&authuser=0",
+	"6209":"http://bato.to/group/_/h/horsey-the-puny-r4725",
+	"6217":"http://krytykal.org/",
+	"6282":"https://binhjamin.wordpress.com/",
+	"6291":"https://yourewelcomescans.wordpress.com/",
+	"6293":"https://sites.google.com/site/sekaigameoredake/",
+	"6294":"http://bato.to/group/_/b/biamam-scans-r4871",
+	"6295":"http://bato.to/group/_/n/nihilsciens-r4879",
+	"6296":"http://ohanashimi.wordpress.com/",
+	"6297":"https://hatoken.wordpress.com/",
+	"6298":"http://last-heaven-fansub.com/",
+	"6300":"http://himetranslations.tumblr.com/",
+	"6301":"http://luciarosescans.blogspot.com/",
+	"6303":"http://pirateyoshi.wordpress.com/",
+	"6305":"http://bato.to/group/_/p/peach-scanlations-r4897",
+	"6306":"http://www.gkamateurs.tumblr.com/",
+	"6308":"http://gospel.みんな/",
+	"6310":"http://neeyuki.tumblr.com/"
 };
 
 var tempIRC = {
