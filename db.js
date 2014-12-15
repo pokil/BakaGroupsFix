6f99b9c193291b71ace55c7e2d7e9f2a0b5e6a34
pokil <cradfire@gmail.com>
2014-12-15 15:43:52 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 var tempGroup = {
+	"5759":"http://skythewood.blogspot.com/",
 	"5777":"https://www.facebook.com/Midnightdawnscans",
 	"6157":"http://finemang.comuf.com/",
+	"6194":"http://argovia2012.wix.com/lastgame",
 	"6198":"http://bato.to/group/_/t/tsuki-to-chikyuu-translations-r4673",
+	"6203":"http://darksequence.wordpress.com/",
 	"6208":"https://drive.google.com/open?id=0B7PIRwu9hTEFODllMFFRemljMGc&authuser=0",
 	"6209":"http://bato.to/group/_/h/horsey-the-puny-r4725",
 	"6217":"http://krytykal.org/",
@@ -19,7 +22,13 @@ var tempGroup = {
 	"6305":"http://bato.to/group/_/p/peach-scanlations-r4897",
 	"6306":"http://www.gkamateurs.tumblr.com/",
 	"6308":"http://gospel.みんな/",
-	"6310":"http://neeyuki.tumblr.com/"
+	"6310":"http://neeyuki.tumblr.com/",
+	"6312":"http://bato.to/group/_/o/operation-gst%C3%B6rte-esel-r4927",
+	"6313":"http://memory-lane-scans.blogspot.ca/",
+	"6315":"http://jayminator.blogspot.de/",
+	"6316":"http://melllllly.tumblr.com/",
+	"6321":"http://www.dis-use.tumblr.com/",
+	"6322":"https://birdycephonaltera.wordpress.com/"
 };
 
 var tempIRC = {
