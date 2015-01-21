d7d4313bb6e54929aedc98d3cf43ea694cac1e49
pokil <cradfire@gmail.com>
2015-01-21 17:45:59 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
 var tempGroup = {
-
+	"5833":"http://holdmyfeels.tumblr.com",
+	"6351":"http://fast-moon.tumblr.com/",
+	"6352":"http://bato.to/group/_/s/sakubelibelles-r5002",
+	"6353":"http://valryssian.wordpress.com/", 
+	"6357":"http://sousetsuka.blogspot.com/",
+	"6359":"http://fehyesvintagemanga.tumblr.com/",
+	"6361":"http://popiscans.com/",
+	"6364":"http://bubblemilkgreenteascans.blogspot.com/",
+	"6372":"http://sushiscans.tumblr.com/",
+	"6373":"https://oniichanyamete.wordpress.com/"
 };
 
 var tempIRC = {
