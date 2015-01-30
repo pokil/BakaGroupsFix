8a1a6a27550c5ea0e32ba08c0928f187a6e35099
pokil <cradfire@gmail.com>
2015-01-30 23:28:40 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,32 @@
 var tempGroup = {
 	"5833":"http://holdmyfeels.tumblr.com",
 	"6118":"http://bato.to/group/_/s/smc-scans-r4402",
+	"6343":"https://www.facebook.com/ethankingdomscans",
 	"6351":"http://fast-moon.tumblr.com/",
 	"6352":"http://bato.to/group/_/s/sakubelibelles-r5002",
 	"6353":"http://valryssian.wordpress.com/",
 	"6357":"http://sousetsuka.blogspot.com/",
-	"6358":"https://seiaiscans.wordpress.com/", 
+	"6358":"https://seiaiscans.wordpress.com/",
 	"6359":"http://fehyesvintagemanga.tumblr.com/",
 	"6360":"http://bato.to/group/_/s/subby-d-scanlations-r5021",
 	"6361":"http://popiscans.com/",
 	"6364":"http://bubblemilkgreenteascans.blogspot.com/",
 	"6367":"https://akashalink.wordpress.com/",
+	"6370":"http://dead-scans.blogspot.com/",
 	"6372":"http://sushiscans.tumblr.com/",
 	"6373":"https://oniichanyamete.wordpress.com/",
-	"6376":"http://addleations.tumblr.com/",	
+	"6376":"http://addleations.tumblr.com/",
 	"6377":"http://bato.to/group/_/r/rooftop-jazz-r3208",
 	"6379":"http://traumereiscans.tumblr.com/",
 	"6380":"http://breadscans.tumblr.com/",
+	"6381":"http://nekojiruproject.blogspot.com/",
+	"6382":"http://wizscans.co.vu/",
 	"6387":"https://arkmachinetranslations.wordpress.com/",
-	"6388":"https://mahoutsuki.wordpress.com/"
+	"6388":"https://mahoutsuki.wordpress.com/",
+	"6390":"https://wakga.wordpress.com/",
+	"6394":"https://gravitytranslations.wordpress.com/",
+	"6395":"http://www.dustloop.com/forums/",
+	"6396":"https://www.mangaholics.net/"
 };
 
 var tempIRC = {
